971a677368e5ee619f491bc5d84c93defc10fe7f
sriniitsme@hotmail.com <sriniitsme@hotmail.com>
2026-04-11 11:42:37 +0530

    Removing debugging logs in Loginpage

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -20,11 +20,6 @@ export class LoginPage extends BasePage {
 
     async login(username: string, password: string) {
 
-        console.log("USERNAME LENGTH:", username.length);
-        console.log("PASSWORD LENGTH:", password.length);
-
-        console.log("USERNAME STARTS WITH:", username.substring(0, 4));
-
         await this.usernameInput.fill(username);
         await this.passwordInput.fill(password);
 
