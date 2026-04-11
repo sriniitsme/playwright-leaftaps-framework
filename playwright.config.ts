2ccc49ee081891d947b64203f3c10eb399663415
sriniitsme@hotmail.com <sriniitsme@hotmail.com>
2026-04-11 12:03:03 +0530

    Reports added to playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,28 +1,14 @@
 import { defineConfig } from '@playwright/test';
-
 export default defineConfig({
   retries: 2,
-
   fullyParallel: true,
-
   workers: process.env.CI ? 2 : undefined,
+
+  reporter: [['html', { open: 'never' }]],
 
   use: {
     baseURL: process.env.BASE_URL || 'https://leaftaps.com/opentaps/control/login',
-
     headless: process.env.CI ? true : false,
-
-    browserName: 'chromium',
-
-    launchOptions: {
-      args: [
-        '--disable-notifications',
-        '--disable-save-password-bubble',
-        '--disable-infobars',
-        '--disable-extensions'
-      ]
-    },
-
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure'
