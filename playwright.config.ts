7d0ea9667528aa0ecc3d1bdc288b39a9b0d1d213
sriniitsme@hotmail.com <sriniitsme@hotmail.com>
2026-04-11 14:58:46 +0530

    Playwright file updated to accomdate QE/DEV/PROD environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from '@playwright/test';
+import * as dotenv from 'dotenv';
+dotenv.config({
+  path: `.env.${process.env.TEST_ENV || 'qa'}`
+});
+
+console.log("ENV:", process.env.TEST_ENV);
+console.log("BASE_URL:", process.env.BASE_URL);
+
 export default defineConfig({
   retries: 2,
   fullyParallel: true,
   workers: process.env.CI ? 2 : undefined,
 
   reporter: [
-    ['line'],
-    ['html', { open: 'never', outputFolder: 'playwright-report' }]
-  ],
+  ['line'],
+  ['allure-playwright']
+],
 
   use: {
     baseURL: process.env.BASE_URL || 'https://leaftaps.com/opentaps/control/login',
